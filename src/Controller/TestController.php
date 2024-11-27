5e8a1916d2795640e19514cd75fe698efdaf3082
asdozzzz <asdozzz89@gmail.com>
2024-11-27 21:53:48 +0500

    asd

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,14 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/test", name: "test")]
 final class TestController extends AbstractController
 {
     public function __construct(private string $appTitle)
     {
     }
 
-    #[Route('/', name: 'index')]
+    #[Route('/test', name: 'index')]
     function test()
     {
         die("<pre>" . print_r('test222 '.$this->appTitle, true) . "</pre>");
